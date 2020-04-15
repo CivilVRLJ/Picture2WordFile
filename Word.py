054f1fb97b14af67a93dd96c54722eb6290d65e9
CivilVRLJ <33928536+CivilVRLJ@users.noreply.github.com>
2020-04-15 17:01:40 +0200

    Update Word.py
    New upload

--- Word.py
+++ Word.py
@@ -1,2 +1,34 @@
 from docx import Document
-f
+from docx.shared import Cm
+import os
+import numpy as np
+
+title = "Erosion vs Height"
+
+document = Document()
+document.add_heading(str(title), 0)
+
+areaList = os.listdir(str(title))
+print("areaList: " + str(areaList))
+
+if ".DS_Store" in areaList:
+    areaList.remove(".DS_Store")
+
+areaList = np.sort(areaList)
+
+for area in areaList:
+
+    listFiles = os.listdir(str(title) + "/" + str(area))
+    listFiles = np.sort(listFiles)
+
+    listImage = []
+    for file in listFiles:
+        print("file: " + str(file))
+        listImage.append(file)
+
+    document.add_heading(area, level=1)
+    for pngName in listImage:
+        document.add_picture(str(title) + "/" + str(area) + "/" + str(pngName), width=Cm(20))
+        document.add_page_break()
+
+document.save(str(title) + '.docx')
